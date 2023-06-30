f7d5923bed3f18853ec3e5c9c2b8d7c1d0423447
Marcos Azevedo (psylinux) <psylinux@gmail.com>
2023-06-30 06:23:15 +0000

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div>
-            <h1>Welcome to the Tab News (Clone) Home Page!</h1>
+            <h1>Welcome to the Tab News (Clone)</h1>
             <p>Why did the chicken cross the road?</p>
             <p>To show the skunk it could be done!</p>
         </div>
