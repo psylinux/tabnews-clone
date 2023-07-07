95cc161e5891c258ba379bb0868af704a4bb98d1
Marcos Azevedo (psylinux) <psylinux@gmail.com>
2023-07-07 17:42:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import { Analytics } from '@vercel/analytics/react';
 
 function Home() {
-    return (
-        <div>
-            <h1>Welcome to the Tab News (Clone)</h1>
-            <p>Why did the chicken cross the road?</p>
-            <p>
-                <marquee>To show the skunk it could be done!</marquee>
-            </p>
-            <Analytics />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome to the Tab News (Clone)</h1>
+      <p>Why did the chicken cross the road?</p>
+      <p>
+        <marquee>To show the skunk it could be done!</marquee>
+      </p>
+      <Analytics />
+    </div>
+  );
 }
 
 export default Home;
